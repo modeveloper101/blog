c88b0e319b8a27fd199315fd88f0fc9f3615d149
Mohamed Rashwan <morashwan8@gmail.com>
2024-02-02 17:25:37 +0700

    update title color

--- src/pages/Home/UsersSection.js
+++ src/pages/Home/UsersSection.js
@@ -6,11 +6,11 @@ const UsersSection = () => {
   return (
     <section className="flex flex-col overflow-hidden w-full bg-[#010101] text-[#010101] pt-[3rem]">
 
-        <h2 className="font-[300] text-6xl p-5 border-b border-black text-[#E6E4E0]">
+        <h2 className="font-[400] text-6xl p-5 border-b border-black text-[#1E1E1E]">
           Top <br />
           auth
           <span>
-            <div className="w-[72px] h-[30px] border-4 border-[#E6E4E0] rounded-full inline-block"></div>
+            <div className="w-[72px] h-[30px] border-4 border-[#1E1E1E] rounded-full inline-block"></div>
           </span>
           rs
         </h2>
